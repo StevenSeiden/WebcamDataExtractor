b62b40615d9c4f5c39ac418c19006068ce6ba3a0
Steven Seiden <53193445+StevenSeiden@users.noreply.github.com>
2021-06-22 13:29:07 -0500

    First commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Cursor Position Calculator
+
+This project aims to find the location of a user's mouse cursor on their screen by analyzing their eye movement.
+
+Made possible by Antoine Lamé's [Gaze Tracking library.](https://github.com/antoinelame/GazeTracking)
---
+++ main.py
@@ -0,0 +1,35 @@
+import cv2
+from pynput.mouse import Button, Controller
+from gaze_tracking import GazeTracking
+
+gaze = GazeTracking()
+webcam = cv2.VideoCapture(0)
+mouse = Controller()
+output = open("output.txt", "w")
+
+while True:
+    # We get a new frame from the webcam
+    _, frame = webcam.read()
+
+    # We send this frame to GazeTracking to analyze it
+    gaze.refresh(frame)
+
+    frame = gaze.annotated_frame()
+    text = ""
+
+    cv2.putText(frame, text, (90, 60), cv2.FONT_HERSHEY_DUPLEX, 1.6, (147, 58, 31), 2)
+
+    if gaze.horizontal_ratio() is not None and gaze.vertical_ratio() is not None:
+        cv2.arrowedLine(frame, (100, 100), (int(gaze.horizontal_ratio()*200), int(-50+gaze.vertical_ratio()*200)), (0, 255, 0), 9)
+        print(int(gaze.vertical_ratio()))
+
+    currentMousePos = mouse.position
+
+    output.write(str(gaze.horizontal_ratio()) + "," + str(gaze.vertical_ratio()) + "," + str(currentMousePos[0]) + str(currentMousePos[1]) + "\n")
+
+    cv2.imshow("Cursor Position Calculator", frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+output.close()
