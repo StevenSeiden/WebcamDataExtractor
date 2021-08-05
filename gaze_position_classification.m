5ba59a05a7eb025f8be65d5dcbb6b3c6ca67d267
Steven Seiden <53193445+StevenSeiden@users.noreply.github.com>
2021-08-05 15:48:50 -0500

    Gaze position classification now works with variable sized data sets

--- gaze_position_classification.m
+++ gaze_position_classification.m
@@ -2,11 +2,12 @@ clear
 close all
 
 names = ["lookingleft","lookingright"];
-datasize = 30;
-X_train = zeros(datasize*length(names)/2,50);
-Y_train = strings(datasize*length(names)/2,1);
-X_test = zeros(datasize*length(names)/2,50);
-Y_test = strings(datasize*length(names)/2,1);
+datasize = 26;
+datalength = 80;
+X_train = zeros(floor(datasize/2)*length(names),datalength);
+Y_train = strings(floor(datasize/2)*length(names),1);
+X_test = zeros((datasize-floor(datasize/2))*length(names),datalength);
+Y_test = strings((datasize-floor(datasize/2))*length(names),1);
 
 count_train = 0;
 count_test = 0;
@@ -15,21 +16,29 @@ for n = 1:length(names)
     Files = dir(strcat(loadDir,'*.txt'));
     
     for i = 1:datasize
-        data = readtable(strcat(loadDir,Files(i).name));
-        data_x = data{:,1};
-        data_y = data{:,2};
-        if i < 16
+        rawdata = readtable(strcat(loadDir,Files(i).name));
+        data = rawdata{:,2};
+        if i < floor(datasize/2)+1
             count_train = count_train + 1;
-            X_train(count_train,:) = interp1(data_x,[1:28.5/50:29]);
+            temp = interp1(data,[1:length(data)/datalength:length(data)]);
+            if length(temp) < 80
+                temp(1,length(temp)+1:80) = temp(length(temp)); 
+            end
+            X_train(count_train,:) = temp;
             Y_train(count_train,:) = names(n);
         else
             count_test = count_test + 1;
-            X_test(count_test,:) = interp1(data_x,[1:28.5/50:29]);
+            temp = interp1(data,[1:length(data)/datalength:length(data)]);
+            if length(temp) < 80
+                temp(1,length(temp)+1:80) = temp(length(temp)); 
+            end
+            X_test(count_test,:) = temp;
             Y_test(count_test,:) = names(n);
         end
     end
 end
 
-model = fitcknn(X_train,Y_train,'NumNeighbors',3,'Standardize',1);
+% model = fitcknn(X_train,Y_train,'NumNeighbors',3,'Standardize',1);
+model = fitcsvm(X_train,Y_train);
 Y_predict = predict(model,X_test);
 mean(Y_predict == Y_test)
